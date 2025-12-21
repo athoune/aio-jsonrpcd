75c9ffd5a9e24f244aa1e764d2f15190298904b1
Mathieu Lecarme <mathieu@garambrogne.net>
2025-12-22 00:01:34 +0100

    SSE and Wesocket

--- src/websocket-events/__init__.py
+++ src/websocket-events/__init__.py
@@ -1,5 +1,10 @@
 from aiohttp import web
+import aiohttp
 from redis import asyncio as aioredis
+import asyncio
+import json
+
+from aiohttp_sse import sse_response
 
 routes = web.RouteTableDef()
 redis: aioredis.Redis = aioredis.from_url("redis://localhost", decode_responses=True)
@@ -15,6 +20,51 @@ async def hello(request):
     key: str = request.match_info["db"].encode("utf8")
     all: dict[str, str] = await redis.hgetall(key)
     return web.json_response(all)
+
+
+@routes.get("/sub/{db}")
+async def subscribe(request):
+    key: str = request.match_info["db"].encode("utf8")
+    pubsub = redis.pubsub()
+    await pubsub.subscribe(key)
+    async with sse_response(request) as resp:
+        while resp.is_connected():
+            async for response in pubsub.listen():
+                if response["type"] == "message":
+                    await resp.send(response["data"])
+    return resp
+
+
+@routes.get("/ws/{db}")
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    key: str = request.match_info["db"].encode("utf8")
+    pubsub: aioredis.PubSub = redis.pubsub()
+    await pubsub.subscribe(key)
+
+    async def sub():
+        for response in pubsub.listen():
+            message: dict = await pubsub.handle_message(response)
+            print(message)
+            await ws.send_json(message)
+
+    task = asyncio.create_task(sub())
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == "close":
+                await ws.close()
+            else:
+                await ws.send_str(msg.data + "/answer")
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print("ws connection closed with exception %s" % ws.exception())
+
+    print("websocket connection closed")
+    await task
+
+    return ws
 
 
 app = web.Application()
