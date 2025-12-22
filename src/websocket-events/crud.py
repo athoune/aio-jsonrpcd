35d32b67c09532b775b56e5314faf2fd78f81fd2
Mathieu Lecarme <mathieu@garambrogne.net>
2025-12-22 13:27:17 +0100

    CRUD class.

---
+++ src/websocket-events/crud.py
@@ -0,0 +1,27 @@
+from redis import asyncio as aioredis
+import os
+
+
+class CRUD:
+    def __init__(self, redis_url: str) -> None:
+        self.redis: aioredis.Redis = aioredis.from_url(redis_url, decode_responses=True)
+        here = os.path.dirname(__file__)
+        self.lazy_delete: str = open(f"{here}/lazy_delete.lua", "r").read()
+
+    async def put_dict(self, db: str, values: dict) -> None:
+        async with self.redis.pipeline(transaction=True) as pipe:
+            for k, v in values.items():
+                if v is None:
+                    await pipe.hdel(db, k)
+                else:
+                    await pipe.hset(db, k, json.dumps(v))
+            await pipe.publish(db, json.dumps(values))
+            await pipe.execute()
+
+    async def delete(self, db: str, key: str):
+        return await self.redis.eval(
+            self.lazy_delete,
+            1,
+            db,
+            key,
+        )
---
+++ src/websocket-events/server.py
@@ -0,0 +1,97 @@
+from aiohttp import web
+import aiohttp
+from redis import asyncio as aioredis
+import asyncio
+import json
+import os
+from .crud import CRUD
+
+from aiohttp_sse import sse_response
+
+routes = web.RouteTableDef()
+redis: aioredis.Redis = aioredis.from_url("redis://localhost", decode_responses=True)
+print("path:", __file__)
+here = os.path.dirname(__file__)
+lazy_delete: str = open(f"{here}/lazy_delete.lua", "r").read()
+crud: CRUD = CRUD("redis://localhost")
+
+
+@routes.get("/")
+async def home(request):
+    return web.Response(text="Master of events.")
+
+
+@routes.get("/{db}")
+async def db(request):
+    key: str = request.match_info["db"].encode("utf8")
+    all: dict[str, str] = await redis.hgetall(key)
+    return web.json_response(all)
+
+
+@routes.put("/{db}")
+async def db_mput(request):
+    key: str = request.match_info["db"]
+    values: dict = await request.json()
+    await crud.put_dict(key, values)
+    return web.Response()
+
+
+@routes.delete("/{db}/{key}")
+async def db_delete(request):
+    db: str = request.match_info["db"]
+    key: str = request.match_info["key"]
+
+    lazy = await crud.delete(db, key)
+    print("lazy:", lazy)
+    return web.Response()
+
+
+@routes.get("/sub/{db}")
+async def subscribe(request):
+    key: str = request.match_info["db"].encode("utf8")
+    pubsub = redis.pubsub()
+    await pubsub.subscribe(key)
+    async with sse_response(request) as resp:
+        while resp.is_connected():
+            async for response in pubsub.listen():
+                if response["type"] == "message":
+                    await resp.send(response["data"])
+    return resp
+
+
+@routes.get("/ws/{db}")
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    key: str = request.match_info["db"].encode("utf8")
+    pubsub: aioredis.PubSub = redis.pubsub()
+    await pubsub.subscribe(key)
+
+    async def sub():
+        for response in pubsub.listen():
+            message: dict = await pubsub.handle_message(response)
+            print(message)
+            await ws.send_json(message)
+
+    task = asyncio.create_task(sub())
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == "close":
+                await ws.close()
+            else:
+                await ws.send_str(msg.data + "/answer")
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print("ws connection closed with exception %s" % ws.exception())
+
+    print("websocket connection closed")
+    await task
+
+    return ws
+
+
+def listen():
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app)
