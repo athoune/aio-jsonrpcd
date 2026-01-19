47ec00c22a137027a457056518debb48a06f11ba
Mathieu Lecarme <mathieu@garambrogne.net>
2026-01-19 22:27:43 +0100

    Demo party.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Jsonrpc server
 
+[Jsonrpc specification](https://www.jsonrpc.org/specification)
+
+## Testing it
+
+Server:
+
+    python -m ws_jsonrpc.rpc.hello
+
+Client:
+
+The client is almost interactive, you have to write your message, a JSON, in this demo.
+
+    python client.py http://0.0.0.0:8080/rpc
+    -> {"method":"hello","id":42, "jsonrpc":"2.0", "params":["bob"]}
---
+++ ws_jsonrpc/rpc/hello.py
@@ -0,0 +1,33 @@
+from aiohttp import web
+
+from .app import App as WsApp, Request, anonymous
+from .web import JsonRpcWebHandler
+
+
+ws_app = WsApp()
+
+
+@ws_app.handler("hello")
+@anonymous
+async def hello(request: Request) -> str:
+    return f"Hello {request.params[0]}"
+
+
+rpc_app = JsonRpcWebHandler(ws_app)
+
+routes = web.RouteTableDef()
+
+routes.get("/rpc")(rpc_app)
+
+
+@routes.get("/")
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+app = web.Application()
+app.add_routes(routes)
+
+
+if __name__ == "__main__":
+    web.run_app(app)
