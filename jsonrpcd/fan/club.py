90f6175d14a95666a60b1992a4d79566417a1b9a
Mathieu Lecarme <mathieu@garambrogne.net>
2026-01-21 12:24:41 +0100

    Spam hits the fan.

---
+++ jsonrpcd/fan/club.py
@@ -0,0 +1,39 @@
+from typing import cast, Any
+
+import jwt
+
+from ..rpc.app import Request, Room, App, User
+
+
+class Club:
+    def __init__(self, app: App):
+        self._app = app
+        self._rooms = dict[str, Room]()
+        self._secrets = dict[str, str]()
+
+    def register_room(self, name: str, secret: str):
+        "Create a new room, with its secret."
+        room = Room(self._app)
+        self._rooms[name] = room
+        self._secrets[name] = secret
+
+    async def authenticate(self, request: Request):
+        params = cast(dict[str, str], request.params)
+        room_name = params["room"]
+
+        room: Room = self._rooms[room_name]
+        secret: str = self._secrets[room_name]
+        meta: dict[str, Any] = jwt.decode(params["token"], secret, algorithms=["HS256"])
+
+        user = User(meta["login"])
+        user["meta"] = meta
+        room.adduser(user, request.session)
+        request.session.user = user
+        request.session.authenticate()
+
+
+async def event(request: Request):
+    "Send an event to everyone on the room"
+    assert request.id_ is None
+    assert request.user is not None
+    await request.user.room.broadcast(request)
---
+++ jsonrpcd/fan/club_test.py
@@ -0,0 +1,32 @@
+import jwt
+import pytest
+from ..rpc.app import Request, Room, App, User, Session, anonymous
+from ..rpc.app_test import OutTest
+from .club import Club
+
+
+@pytest.mark.asyncio
+async def testClub():
+    app = App()
+    club = Club(app)
+    club.register_room("harry", "potter")
+
+    app.handler("authenticate")(anonymous(club.authenticate))
+
+    token = jwt.encode({"login": "hermione"}, "potter", algorithm="HS256")
+
+    out = OutTest()
+    session = Session(out)
+
+    resp = await app._handle(
+        session,
+        dict(method="authenticate", id=17, params=dict(room="harry", token=token)),
+    )
+
+    assert resp is None
+    assert "hermione" in app._users
+    hermione = session.user
+    assert hermione is not None
+    assert session.authenticated
+    assert session.room is not None
+    assert session.room == club._rooms["harry"]
